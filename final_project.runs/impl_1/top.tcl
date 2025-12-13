69ed7ba31204a83b282499201f370e1acbc5173c
microhum <Guntee12123@gmail.com>
2025-12-13 12:22:31 +0700

    add music player

--- final_project.runs/impl_1/top.tcl
+++ final_project.runs/impl_1/top.tcl
@@ -46,83 +46,11 @@ set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
 set_msg_config  -ruleid {1}  -id {Synth 8-685}  -string {{ERROR: [Synth 8-685] variable 'LED_segment' should not be used in output port connection [C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.srcs/sources_1/new/top.v:20]}}  -suppress 
 
-start_step init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param xicom.use_bs_reader 1
-  set_property design_mode GateLvl [current_fileset]
-  set_property webtalk.parent_dir C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.cache/wt [current_project]
-  set_property parent.project_path C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.xpr [current_project]
-  set_property ip_repo_paths c:/Users/Gunte/Documents/Vivaldo/final_project/final_project.cache/ip [current_project]
-  set_property ip_output_repo c:/Users/Gunte/Documents/Vivaldo/final_project/final_project.cache/ip [current_project]
-  add_files -quiet C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.runs/synth_1/top.dcp
-  read_xdc C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.srcs/constrs_1/new/constr.xdc
-  link_design -top top -part xc7a35tcpg236-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-}
-
-start_step opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  catch {write_debug_probes -quiet -force debug_nets}
-  opt_design 
-  write_checkpoint -force top_opt.dcp
-  report_drc -file top_drc_opted.rpt
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-}
-
-start_step place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  catch {write_hwdef -file top.hwdef}
-  place_design 
-  write_checkpoint -force top_placed.dcp
-  report_io -file top_io_placed.rpt
-  report_utilization -file top_utilization_placed.rpt -pb top_utilization_placed.pb
-  report_control_sets -verbose -file top_control_sets_placed.rpt
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-}
-
-start_step route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force top_routed.dcp
-  report_drc -file top_drc_routed.rpt -pb top_drc_routed.pb
-  report_timing_summary -warn_on_violation -max_paths 10 -file top_timing_summary_routed.rpt -rpx top_timing_summary_routed.rpx
-  report_power -file top_power_routed.rpt -pb top_power_summary_routed.pb
-  report_route_status -file top_route_status.rpt -pb top_route_status.pb
-  report_clock_utilization -file top_clock_utilization_routed.rpt
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-}
-
 start_step write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  open_checkpoint top_routed.dcp
+  set_property webtalk.parent_dir C:/Users/Gunte/Documents/Vivaldo/final_project/final_project.cache/wt [current_project]
   catch { write_mem_info -force top.mmi }
   write_bitstream -force top.bit 
   catch { write_sysdef -hwdef top.hwdef -bitfile top.bit -meminfo top.mmi -file top.sysdef }
